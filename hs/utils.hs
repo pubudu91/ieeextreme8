aa3588d50daf09a2b29c695f4efcd5c2cce152dd
KommuSoft <vanonsem.willem@gmail.com>
2014-10-17 22:20:18 +0200

    ... and another commit ...

--- hs/utils.hs
+++ hs/utils.hs
@@ -6,4 +6,4 @@ solve = map length . evolution next []
 
 evolution :: (Eq a) => (a -> a) -> a -> a -> [a]
 evolution f end x | x == end = []
-                  | otherwise x : (evolution f end (f x))
+                  | otherwise = x : (evolution f end (f x))
