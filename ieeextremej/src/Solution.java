737babf4dc6fe14908b972a8f246fd56f4ec624d
KommuSoft <vanonsem.willem@gmail.com>
2014-10-17 20:13:21 +0200

    ... and another commit ...

---
+++ ieeextremej/src/Solution.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author kommusoft
+ */
+public class Solution {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
