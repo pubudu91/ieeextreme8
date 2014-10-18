db31342b6852672639144e92f75342af9967a37e
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 16:21:08 +0200

    ... and another commit ...

--- hs/siu.hs
+++ hs/siu.hs
@@ -1,4 +1,4 @@
-siu :: [Int] -> Int -> Integer
+siu :: [Int] -> Int -> Int
 siu as n = powm n (foldl summ 0 as)
 
 summ :: Int -> Int -> Int
@@ -7,16 +7,13 @@ summ a b = mod (a+b) 1000000007
 powm :: Int -> Int -> Int
 powm 0 _ = 1
 powm n k | even n = pw
-         | otherwise = div k pw 1000000007
+         | otherwise = mod pw 1000000007
     where nb = div n 2
           kb = mod (k*k) 1000000007
           pw = powm nb kb
-
-solve :: [String] -> String
-solve = siu . map (read)
 
 main = do
     a <- getLine
     b <- getLine
     c <- getLine
-    print (siu (map (read) . words b) (read c))
+    print (siu (map (read :: Int) (words b)) (read c))
