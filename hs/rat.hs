b280c6417b2e9c15b16681ccf4c1e855d890e1fd
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 10:21:01 +0200

    ... and another commit ...

--- hs/rat.hs
+++ hs/rat.hs
@@ -5,7 +5,7 @@ import Data.List
 ferats a = [b|b<-[1..(a-1)],let cc = a*a-b*b, cc > 0, let c = floor ( sqrt ( fromIntegral cc)), b*b<=cc,cc == c*c]
 
 vferats :: Int -> Int -> Int -> Bool
-vferats a b d = trace (show a++","++show b++","++show d) e*b == a*d && cc == c*c
+vferats a b d = e*b == a*d && cc == c*c
     where e = div (a*d) b
           cc = a*a-e*e
           c = floor ( sqrt ( fromIntegral cc))
