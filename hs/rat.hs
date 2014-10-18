e8523efa9c1fb25cba3caeb438d18bdedc873ffa
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 09:28:43 +0200

    ... and another commit ...

--- hs/rat.hs
+++ hs/rat.hs
@@ -1,2 +1,2 @@
 ferats :: [Int]
-ferats = [a|a<-[1..1001],b<-[1..a],let cc = a*a-b*b, let c = floor ( sqrt ( fromIntegral c)), b*b<cc,cc == c*c]
+ferats = [a|a<-[2..1001],b<-[1..(a-1)],let cc = a*a-b*b, let c = floor ( sqrt ( fromIntegral cc)), b*b<cc,cc == c*c]
