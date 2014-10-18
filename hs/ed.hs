50cea0858ed6556ce1c71805680cac3a4a58bd09
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 02:31:09 +0200

    ... and another commit ...

--- hs/ed.hs
+++ hs/ed.hs
@@ -1,7 +1,7 @@
-solve [Int] -> [Int] -> Int
+solve :: [Int] -> [Int] -> Int
 solve [n,m,k] as = n
 
 main = do
     nmk <- getLine
     vls <- getLine
-    print solve (map (read) (words nmk)) (map (read) (words vls))
+    print (solve (map (read) (words nmk)) (map (read) (words vls)))
