873038ac5fca951a36ffbee05599712316c6b9a1
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 16:14:01 +0200

    ... and another commit ...

--- hs/siu.hs
+++ hs/siu.hs
@@ -4,4 +4,7 @@ siu as n = mod (n*(fromIntegral (foldl summ 0 as))) 1000000007
 summ :: Int -> Int -> Int
 summ a b = mod (a+b) 1000000007
 
+solve :: [String] -> String
+solve = siu . map (read)
+
 main = do 
