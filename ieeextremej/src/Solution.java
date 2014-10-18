83625f5c9fa62c19f6950c9d7d8ff7cff471253c
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 13:26:09 +0200

    ... and another commit ...

--- ieeextremej/src/Solution.java
+++ ieeextremej/src/Solution.java
@@ -3,20 +3,18 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-
 /**
  *
  * @author kommusoft
  */
 public class Solution {
-    
+
     private class Item {
-        
+
         public String name;
         public int weight;
         public int value;
-        
-        
+
     }
 
     public static void main(String[] args) {
@@ -27,16 +25,16 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         int r = sc.nextInt();
         int k = sc.nextInt();
-        int tot = r*k;
+        int tot = r * k;
         sc.nextLine();
         ArrayList<Item> itms = new ArrayList<>();
-        Pattern p = Pattern.compile("\\w+");
+        Pattern p = Pattern.compile("[A-Za-z0-9]+");
         Item itm;
-        
-        while(sc.hasNextLine()) {
-            System.out.println();
+
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
             String nm = sc.next(p);
-            if(nm.equals("END")) {
+            if (nm.equals("END")) {
                 break;
             } else {
                 itm = new Item();
