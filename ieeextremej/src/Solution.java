67cf3c6873c8111a8ccf73cb3ce2b4f689b46693
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 17:29:38 +0200

    ... and another commit ...

--- ieeextremej/src/Solution.java
+++ ieeextremej/src/Solution.java
@@ -10,51 +10,51 @@ import java.util.regex.Pattern;
  * @author kommusoft
  */
 public class Solution {
-    
+
     private class Item implements Comparable<Item> {
-        
+
         public String name;
         public int weight;
         public int value;
         public int take;
-        
+
         @Override
         public int compareTo(Item t) {
             return ((Integer) (t.value * weight)).compareTo(t.weight * value);
         }
-        
-        public int totalW () {
-            return this.take*this.weight;
+
+        public int totalW() {
+            return this.take * this.weight;
         }
-        
-        public int totalV () {
-            return this.take*this.value;
+
+        public int totalV() {
+            return this.take * this.value;
         }
-        
-        public double load () {
-            return (double) this.value/this.weight;
+
+        public double load() {
+            return (double) this.value / this.weight;
         }
 
         @Override
         public String toString() {
-            return String.format("%s,%s,%s,%s",this.name,this.take,this.take*this.weight,this.take*this.value);
+            return String.format("%s,%s,%s,%s", this.name, this.take, this.take * this.weight, this.take * this.value);
         }
-        
+
     }
-    
+
     private class Sorter implements Comparator<Item> {
 
         @Override
         public int compare(Item t, Item t1) {
             return t.name.compareTo(t1.name);
         }
-        
+
     }
-    
+
     public static void main(String[] args) {
         new Solution().run();
     }
-    
+
     private void run() {
         Scanner sc = new Scanner(System.in);
         int r = sc.nextInt();
@@ -65,7 +65,7 @@ public class Solution {
         ArrayList<Item> itms = new ArrayList<>();
         Pattern p = Pattern.compile("[^,\n\t\r ]+");
         Item itm;
-        
+
         while (sc.hasNextLine()) {
             String nm = sc.next(p);
             if (nm.toLowerCase().equals("end")) {
@@ -79,25 +79,28 @@ public class Solution {
             }
         }
         Collections.sort(itms);
-        int rem = tot;
-        for(Item itmi : itms) {
-            int wgh = itmi.weight;
-            int take = rem/wgh;
-            itmi.take = take;
-            rem -= wgh*take;
+        int nim = 0x01 << itms.size();
+        for (int i = 0x00; i < nim; i++) {
+            int rem = tot;
+            for (Item itmi : itms) {
+                int wgh = itmi.weight;
+                int take = rem / wgh;
+                itmi.take = take;
+                rem -= wgh * take;
+            }
         }
-        Collections.sort(itms,new Sorter());
+        Collections.sort(itms, new Sorter());
         int v = 0x00;
         int w = 0x00;
-        for(Item itmi : itms) {
-            if(itmi.take > 0) {
+        for (Item itmi : itms) {
+            if (itmi.take > 0) {
                 System.out.println(itmi);
                 w += itmi.totalW();
                 v += itmi.totalV();
             }
         }
-        System.out.println(String.format("%s,%s",w,v));
-        System.out.println(String.format("Each robber gets: %.2f",(double) v/r));
+        System.out.println(String.format("%s,%s", w, v));
+        System.out.println(String.format("Each robber gets: %.2f", (double) v / r));
     }
-    
+
 }
