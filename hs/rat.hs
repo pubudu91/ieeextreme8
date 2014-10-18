b5ec060b27edd91ed3c4beae7e29d9b75c84e526
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 09:31:43 +0200

    ... and another commit ...

--- hs/rat.hs
+++ hs/rat.hs
@@ -2,6 +2,8 @@ ferats :: [Int]
 ferats = [a|a<-[2..1001],b<-[1..(a-1)],let cc = a*a-b*b, let c = floor ( sqrt ( fromIntegral cc)), b*b<cc,cc == c*c]
 
 solve :: [Int] -> String
+solve [a,b] | (elem a ferats) and (elem b ferats) = "TRUE"
+            | otherwise = "FALSE"
 
 main = interact (unlines . map (solve . read) . words . tail . lines)
     
