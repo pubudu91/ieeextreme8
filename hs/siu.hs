e629e5546b83a94d534304de4798601a2f28d3fb
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 16:13:01 +0200

    ... and another commit ...

--- hs/siu.hs
+++ hs/siu.hs
@@ -1,5 +1,7 @@
-siu :: [Int] -> Int -> Int
-siu as n = (foldl summ 0 as)
+siu :: [Int] -> Int -> Integer
+siu as n = mod (n*(fromIntegral (foldl summ 0 as))) 1000000007
 
 summ :: Int -> Int -> Int
 summ a b = mod (a+b) 1000000007
+
+main = do 
