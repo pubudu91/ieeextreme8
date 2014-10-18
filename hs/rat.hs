4646685d8886267d98bc852afe86c58c80d6e0fe
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 09:41:47 +0200

    ... and another commit ...

--- hs/rat.hs
+++ hs/rat.hs
@@ -1,12 +1,12 @@
-ferats :: [Int]
+--ferats :: [(Int]
 ferats = [(a,b)|a<-[2..1001],b<-[1..(a-1)],let cc = a*a-b*b, let c = floor ( sqrt ( fromIntegral cc)), b*b<cc,cc == c*c]
 
-solve :: [String] -> String
-solve = solve' . map (read)
+--solve :: [String] -> String
+--solve = solve' . map (read)
 
-solve' :: [Int] -> String
-solve' [a,b] | (elem a ferats) && (elem b ferats) = "TRUE"
-             | otherwise = "FALSE"
+--solve' :: [Int] -> String
+--solve' [a,b] | (elem a ferats) && (elem b ferats) = "TRUE"
+--             | otherwise = "FALSE"
 
-main = interact (unlines . map (solve . words) . tail . lines)
+--main = interact (unlines . map (solve . words) . tail . lines)
     
