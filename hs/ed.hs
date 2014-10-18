7115b2d23958580a238ea624e94888819278c41b
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 03:41:43 +0200

    ... and another commit ...

--- hs/ed.hs
+++ hs/ed.hs
@@ -3,7 +3,7 @@ solve [_,m,k] as = solve' k l (drop k l)
     where l = (as++take (k-1) as)
 
 softOrdering :: Int -> Int64 -> Ordering
-softOrdering 	
+softOrdering a b = compare ()
 
 solve' :: Int -> [Int] -> [Int]
 
