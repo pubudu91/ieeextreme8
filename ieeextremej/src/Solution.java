e2fe8c7fd9975c0d98127bb64734db9f445b2adc
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 16:50:18 +0200

    ... and another commit ...

--- ieeextremej/src/Solution.java
+++ ieeextremej/src/Solution.java
@@ -1,11 +1,16 @@
+
 /**
  *
  * @author kommusoft
  */
 public class Solution {
-    
-    public static void main (String[] args) {
-        
+
+    public static void main(String[] args) {
+        new Solution().run();
     }
-    
+
+    private void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
