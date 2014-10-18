d9673ee4493e7c97422433e922353189783478d7
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 04:02:53 +0200

    ... and another commit ...

--- hs/ed.hs
+++ hs/ed.hs
@@ -1,15 +1,16 @@
 import Data.Tree
 
-type State = (AVL Int,Int,AVL Int)
+type State = (AVL (Int,Int),Int,AVL (Int,Int))
 
-solve :: [Int] -> [Int] -> Int
-solve [_,m,k] as = solve' k l (drop k l)
-    where l = (as++take (k-1) as)
+--solve :: [Int] -> [Int] -> Int
+--solve [_,m,k] as = solve' k l (drop k l)
+--    where l = (as++take (k-1) as)
 
 initialize :: Int -> [Int] -> State
+initialize k as = 
+    
 
-
-main = do
-    nmk <- getLine
-    vls <- getLine
-    print (solve (map (read) (words nmk)) (map (read) (words vls)))
+--main = do
+--    nmk <- getLine
+--    vls <- getLine
+--    print (solve (map (read) (words nmk)) (map (read) (words vls)))
