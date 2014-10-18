8b23983dd06a8f589d2e422ed3500f0e7e5f63f0
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 05:11:17 +0200

    ... and another commit ...

--- ieeextremej/src/Solution.java
+++ ieeextremej/src/Solution.java
@@ -24,10 +24,12 @@ public class Solution {
         int m = sc.nextInt();
         this.n = sc.nextInt();
         long so = sc.nextLong(), s = so+0x01;
-        for(int i = 0x01; i <= m; i++) {
-            printState(i,s,n);
-            s = next(s);
+        for(int i = 0x01; i < m || so == s; i++) {
+            printState(i,so,n);
+            s = so;
+            so = next(s);
         }
+        printState(i,s,n);
     }
     
     private long next (long s) {
