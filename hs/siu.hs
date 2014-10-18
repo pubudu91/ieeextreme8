980fa8e2ab973bb85e08391ffbdb40c6af9fd8b9
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 16:16:01 +0200

    ... and another commit ...

--- hs/siu.hs
+++ hs/siu.hs
@@ -4,6 +4,11 @@ siu as n = mod (n*(fromIntegral (foldl summ 0 as))) 1000000007
 summ :: Int -> Int -> Int
 summ a b = mod (a+b) 1000000007
 
+powm :: Int -> Int -> Int
+powm 0 _ = 1
+powm n k | even n = mod (pow nb k) 1000000007
+    where 
+
 solve :: [String] -> String
 solve = siu . map (read)
 
