f62a9209365cc39fd8b25091c37edbbcccd9ef6d
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 09:53:51 +0200

    ... and another commit ...

--- hs/rat.hs
+++ hs/rat.hs
@@ -3,19 +3,19 @@ import Data.List
 --ferats :: [Int]
 ferats a = nub [b|b<-[1..(a-1)],let cc = a*a-b*b, let c = floor ( sqrt ( fromIntegral cc)), b*b<cc,cc == c*c]
 
-vferats :: Int -> Int -> Int
+vferats :: Int -> Int -> Bool
 vferats a b = cc == c*c
     where cc = a*a-b*b
           c = floor ( sqrt ( fromIntegral cc))
 
-solve :: Int -> Int -> Bool
-solve a b = any (vferats b) (ferats a)
+solve'' :: Int -> Int -> Bool
+solve'' a b = any (vferats b) (ferats a)
 
 solve :: [String] -> String
 solve = solve' . map (read)
 
---solve' :: [Int] -> String
---solve' [a,b] | (elem a ferats) && (elem b ferats) = "TRUE"
---             | otherwise = "FALSE"
+solve' :: [Int] -> String
+solve' [a,b] | solve'' a b = "TRUE"
+             | otherwise = "FALSE"
 
---main = interact (unlines . map (solve . words) . tail . lines)
+main = interact (unlines . map (solve . words) . tail . lines)
