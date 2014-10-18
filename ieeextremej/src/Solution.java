6531165f3b30630798edc17aa68bf6942cb6c25f
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 16:56:18 +0200

    ... and another commit ...

--- ieeextremej/src/Solution.java
+++ ieeextremej/src/Solution.java
@@ -1,28 +1,28 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
-
 
 /**
  *
  * @author kommusoft
  */
 public class Solution {
-    
+
     private class FS implements Comparable<FS> {
-        
+
         public int dist;
         public int cost;
-        
-        public FS (int dist, int cost) {
+
+        public FS(int dist, int cost) {
             this.dist = dist;
             this.cost = cost;
         }
 
         @Override
         public int compareTo(FS t) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            return ((Integer) this.dist).compareTo(t.dist);
         }
-        
+
     }
 
     public static void main(String[] args) {
@@ -32,14 +32,15 @@ public class Solution {
     private void run() {
         Scanner sc = new Scanner(System.in);
         int test = sc.nextInt();
-        for(; test > 0; test--) {
+        ArrayList<FS> fss = new ArrayList<>();
+        for (; test > 0; test--) {
             int nGas = sc.nextInt();
             int full = sc.nextInt();
             int start = sc.nextInt();
             int length = sc.nextInt();
-            
-            for(int i = 0; i < nGas; i++) {
-                
+
+            for (int i = 0; i < nGas; i++) {
+
             }
         }
     }
