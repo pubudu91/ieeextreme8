b69ad29992ea5ad54ce899c3de668e548e4c5a45
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 09:30:43 +0200

    ... and another commit ...

--- hs/rat.hs
+++ hs/rat.hs
@@ -1,5 +1,7 @@
 ferats :: [Int]
 ferats = [a|a<-[2..1001],b<-[1..(a-1)],let cc = a*a-b*b, let c = floor ( sqrt ( fromIntegral cc)), b*b<cc,cc == c*c]
 
-main = 
+solve :: [Int] -> String
+
+main = interact (unlines . map (solve . read) . words . tail . lines)
     
