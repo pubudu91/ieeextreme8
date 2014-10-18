64cdf40e2de2841a5e8696091691193d66c0c825
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 03:22:39 +0200

    ... and another commit ...

--- hs/ed.hs
+++ hs/ed.hs
@@ -2,7 +2,7 @@ solve :: [Int] -> [Int] -> Int
 solve [_,m,k] as = solve' k l (drop k l)
     where l = (as++take (k-1) as)
 
-solve' :: Int -> [Int]
+solve' :: Int -> [Int] -> [Int] -> 
 
 main = do
     nmk <- getLine
