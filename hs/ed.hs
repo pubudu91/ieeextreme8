51589d061da8b88365480f6635fd146a2329739c
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 03:46:46 +0200

    ... and another commit ...

--- hs/ed.hs
+++ hs/ed.hs
@@ -1,9 +1,8 @@
+type (Hea)
+
 solve :: [Int] -> [Int] -> Int
 solve [_,m,k] as = solve' k l (drop k l)
     where l = (as++take (k-1) as)
-
-softOrdering :: Int -> Int64 -> Ordering
-softOrdering a b = compare a (shift b 32)
 
 solve' :: Int -> [Int] -> [Int]
 
