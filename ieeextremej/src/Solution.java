0da96d84f8371078a9be292aa608ae1048754a6f
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 13:01:59 +0200

    ... and another commit ...

---
+++ ieeextremej/src/Solution.java
@@ -0,0 +1,17 @@
+
+/**
+ *
+ * @author kommusoft
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution().run();
+    }
+
+    private void run() {
+        Scanner sc = new Scanner();
+        
+    }
+
+}
