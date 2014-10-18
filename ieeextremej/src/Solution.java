6d519a97ec2165399eaf4b7ddb52e31548c7320a
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 12:03:38 +0200

    ... and another commit ...

--- ieeextremej/src/Solution.java
+++ ieeextremej/src/Solution.java
@@ -72,6 +72,7 @@ public class Solution {
             int left = Math.max(0, hleft);
             need = Math.min(dist - left, this.full);
             int c = need * fsc.c;
+            System.out.println(""+need+"x"+fsc.c);
             if (hleft < left) {
                 c += solve(f0, from, index);
             }
