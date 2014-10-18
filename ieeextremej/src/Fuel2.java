ebfabee9bb1bd8ec5566c43c9b1a76eea86ad102
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 17:28:38 +0200

    ... and another commit ...

---
+++ ieeextremej/src/Fuel2.java
@@ -0,0 +1,113 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/**
+ *
+ * @author kommusoft
+ */
+public class Fuel2 {
+
+    private ArrayList<FS> fss;
+    private int full;
+    private int split;
+
+    private int solve(int fuel, int offset, int length, int from, int to) throws Exception {
+        if (fuel >= length) {
+            return 0;
+        } else {
+            FS fsm = minimum(from, to);
+            if (fsm == null) {
+                throw new Exception();
+            }
+            int idx = this.split;
+            int x01 = fsm.dist;
+            int d0 = x01 - offset;
+            int d1 = length + offset - x01;//the total amount of fuel needed
+            int need = Math.min(d1, this.full);
+            int left = Math.max(fuel - d0, 0);
+            int tank = need - left;
+            int c = tank * fsm.cost;
+            if (d0 > fuel) {
+                c += solve(fuel, offset, d0, from, idx);
+            }
+            if (d1 > need) {
+                c += solve(need, x01, d1, idx + 0x01, to);
+            }
+            return c;
+        }
+    }
+
+    private FS minimum(int from, int to) {
+        int minc = Integer.MAX_VALUE;
+        int mini = -1;
+        FS minf = null;
+        for (int i = to - 0x01; i >= from; i--) {
+            FS fsi = fss.get(i);
+            if (fsi.cost < minc) {
+                minf = fsi;
+                mini = i;
+                minc = fsi.cost;
+            }
+        }
+        this.split = mini;
+        return minf;
+    }
+
+    private class FS implements Comparable<FS> {
+
+        public int dist;
+        public int cost;
+
+        public FS(int dist, int cost) {
+            this.dist = dist;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(FS t) {
+            return ((Integer) this.dist).compareTo(t.dist);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new Fuel2().run();
+    }
+
+    private void run() {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        for (; test > 0; test--) {
+            solveTest(sc);
+
+        }
+    }
+
+    private void solveTest(Scanner sc) {
+        ArrayList<FS> fss = new ArrayList<>();
+        int nGas = sc.nextInt();
+        int full = sc.nextInt();
+        int start = sc.nextInt();
+        int length = sc.nextInt();
+
+        for (int i = 0; i < nGas; i++) {
+            int di = sc.nextInt();
+            int ci = sc.nextInt();
+            if (di < length) {
+                fss.add(new FS(di, ci));
+            }
+        }
+        Collections.sort(fss);
+        this.fss = fss;
+        this.full = full;
+        try {
+            int c = solve(start, 0, length, 0x00, fss.size());
+            System.out.println(c);
+        } catch (Exception ex) {
+            System.out.println(-1);
+        }
+    }
+
+}
--- ieeextremej/src/Solution.java
+++ ieeextremej/src/Solution.java
@@ -1,115 +1,103 @@
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author kommusoft
  */
 public class Solution {
-
-    private ArrayList<FS> fss;
-    private int full;
-    private int split;
-
-    private int solve(int fuel, int offset, int length, int from, int to) throws Exception {
-        if (fuel >= length) {
-            return 0;
-        } else {
-            FS fsm = minimum(from, to);
-            if (fsm == null) {
-                throw new Exception();
-            }
-            int idx = this.split;
-            int x01 = fsm.dist;
-            int d0 = x01 - offset;
-            int d1 = length + offset - x01;//the total amount of fuel needed
-            int need = Math.min(d1, this.full);
-            int left = Math.max(fuel - d0, 0);
-            int tank = need - left;
-            int c = tank * fsm.cost;
-            if (d0 > fuel) {
-                c += solve(fuel, offset, d0, from, idx);
-            }
-            if (d1 > need) {
-                c += solve(need, x01, d1, idx+0x01, to);
-            }
-            return c;
+    
+    private class Item implements Comparable<Item> {
+        
+        public String name;
+        public int weight;
+        public int value;
+        public int take;
+        
+        @Override
+        public int compareTo(Item t) {
+            return ((Integer) (t.value * weight)).compareTo(t.weight * value);
         }
-    }
-
-    private FS minimum(int from, int to) {
-        int minc = Integer.MAX_VALUE;
-        int mini = -1;
-        FS minf = null;
-        for (int i = to-0x01; i >= from; i--) {
-            FS fsi = fss.get(i);
-            if (fsi.cost < minc) {
-                minf = fsi;
-                mini = i;
-                minc = fsi.cost;
-            }
+        
+        public int totalW () {
+            return this.take*this.weight;
         }
-        this.split = mini;
-        return minf;
-    }
-
-    private class FS implements Comparable<FS> {
-
-        public int dist;
-        public int cost;
-
-        public FS(int dist, int cost) {
-            this.dist = dist;
-            this.cost = cost;
+        
+        public int totalV () {
+            return this.take*this.value;
+        }
+        
+        public double load () {
+            return (double) this.value/this.weight;
         }
 
         @Override
-        public int compareTo(FS t) {
-            return ((Integer) this.dist).compareTo(t.dist);
+        public String toString() {
+            return String.format("%s,%s,%s,%s",this.name,this.take,this.take*this.weight,this.take*this.value);
         }
-
+        
     }
+    
+    private class Sorter implements Comparator<Item> {
 
+        @Override
+        public int compare(Item t, Item t1) {
+            return t.name.compareTo(t1.name);
+        }
+        
+    }
+    
     public static void main(String[] args) {
         new Solution().run();
     }
-
+    
     private void run() {
         Scanner sc = new Scanner(System.in);
-        int test = sc.nextInt();
-        for (; test > 0; test--) {
-            solveTest(sc);
-
-        }
-    }
-
-    private void solveTest(Scanner sc) {
-        ArrayList<FS> fss = new ArrayList<>();
-        int nGas = sc.nextInt();
-        int full = sc.nextInt();
-        int start = sc.nextInt();
-        int length = sc.nextInt();
-
-        for (int i = 0; i < nGas; i++) {
-            int di = sc.nextInt();
-            int ci = sc.nextInt();
-            if (di < length) {
-                fss.add(new FS(di, ci));
+        int r = sc.nextInt();
+        int k = sc.nextInt();
+        int tot = r * k;
+        sc.nextLine();
+        sc.useDelimiter("[\n\t\r, ]+");
+        ArrayList<Item> itms = new ArrayList<>();
+        Pattern p = Pattern.compile("[^,\n\t\r ]+");
+        Item itm;
+        
+        while (sc.hasNextLine()) {
+            String nm = sc.next(p);
+            if (nm.toLowerCase().equals("end")) {
+                break;
+            } else {
+                itm = new Item();
+                itm.name = nm;
+                itm.weight = sc.nextInt();
+                itm.value = sc.nextInt();
+                itms.add(itm);
             }
         }
-        Collections.sort(fss);
-        this.fss = fss;
-        this.full = full;
-        try {
-            int c = solve(start, 0, length, 0x00, fss.size());
-            System.out.println(c);
-        } catch (Exception ex) {
-            System.out.println(-1);
+        Collections.sort(itms);
+        int rem = tot;
+        for(Item itmi : itms) {
+            int wgh = itmi.weight;
+            int take = rem/wgh;
+            itmi.take = take;
+            rem -= wgh*take;
         }
+        Collections.sort(itms,new Sorter());
+        int v = 0x00;
+        int w = 0x00;
+        for(Item itmi : itms) {
+            if(itmi.take > 0) {
+                System.out.println(itmi);
+                w += itmi.totalW();
+                v += itmi.totalV();
+            }
+        }
+        System.out.println(String.format("%s,%s",w,v));
+        System.out.println(String.format("Each robber gets: %.2f",(double) v/r));
     }
-
+    
 }
