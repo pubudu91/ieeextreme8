5ba83f08e8a9151d6649cba7224dabfaf9a66b48
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 16:24:08 +0200

    ... and another commit ...

--- hs/siu.hs
+++ hs/siu.hs
@@ -7,7 +7,7 @@ summ a b = mod (a+b) 1000000007
 powm :: Int -> Int -> Int
 powm 0 _ = 1
 powm n k | even n = pw
-         | otherwise = mod pw 1000000007
+         | otherwise = mod (k*pw) 1000000007
     where nb = div n 2
           kb = mod (k*k) 1000000007
           pw = powm nb kb
