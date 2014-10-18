72d46ff9bd110c7c8cc620b27663b0a7be35b671
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 04:21:56 +0200

    ... and another commit ...

--- ieeextremej/src/Solution.java
+++ ieeextremej/src/Solution.java
@@ -21,11 +21,16 @@ public class Solution {
         int m = sc.nextInt();
         int n = sc.nextInt();
         long s = sc.nextLong();
-        printState(s,n);
+        printState(1,s,n);
         
     }
     
     private static void printState (int it, long state, int n) {
+        String num = ""+it;
+        System.out.print(num);
+        for(int i = num.length(); i < 3; i++) {
+            System.out.print(' ');
+        }
         System.out.print('-');
         for(long mask = 0x1l<<(n-0x01); mask != 0x00; mask >>= 0x01) {
             if((state&mask) != 0x00) {
