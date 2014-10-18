fb1bf5e7485a5d1d0dd7f8cd2b9b6020f7a41730
Roald Frederickx <roald.frederickx@gmail.com>
2014-10-18 18:17:56 +0200

    more tries

--- tries/Solution.java
+++ tries/Solution.java
@@ -14,20 +14,36 @@ public class Solution {
             
             long bytesPerNode = 4 * A;
 
+			if (A == 1) {
+				System.out.println(bytesPerNode * N);
+				continue;
+			}
+
 			double exponent = Math.floor(Math.log((double)N) / Math.log((double)A)); // TODO: + epsilon before floor?
-			System.out.println("exponent "+exponent);
+			//System.out.println("exponent "+exponent);
             long expandingDepth = (long)(0.5 + exponent); // depth of purely exponentially expanding part, including the root node
-			System.out.println("expandingDepth "+expandingDepth);
+			//System.out.println("expandingDepth "+expandingDepth);
             if (expandingDepth >= L) {
-                long numNodes = (long) + (0.5 + (Math.pow((double)A, (double)L) - 1) / (double)(A - 1));
-				System.out.println("numNodes "+numNodes);
+                long numNodes = (long) + (0.5 + (Math.pow((double)A, (double)L+1) - 1) / (double)(A - 1));
+				//System.out.println("numNodes "+numNodes);
                 System.out.println(numNodes * bytesPerNode);
                 continue;
             }
 			long numExpanding = (long) (0.5 + (Math.pow((double)A, exponent+1) - 1) / (double)(A - 1));
-			System.out.println("numExpanding "+numExpanding);
-            long numFixedWidth = (long) (L - expandingDepth) * N;
-			System.out.println("numFixedWidth "+numFixedWidth);
+			//System.out.println("numExpanding "+numExpanding);
+            long fixedWidthDepth;
+
+			fixedWidthDepth = (long) (L - expandingDepth);
+			/*
+			if (expandingDepth == 0)
+				fixedWidthDepth = (long) (L - expandingDepth);
+			else 
+				fixedWidthDepth = (long) (L - expandingDepth - 1);
+				*/
+
+			//System.out.println("fixedWidthDepth "+fixedWidthDepth);
+            long numFixedWidth = fixedWidthDepth * N;
+			//System.out.println("numFixedWidth "+numFixedWidth);
 
 			System.out.println((numExpanding + numFixedWidth) * bytesPerNode);
 		}
