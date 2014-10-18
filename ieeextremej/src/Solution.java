82eea5d65ee5647bdef76db0b348b274c8aa0030
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 16:51:18 +0200

    ... and another commit ...

--- ieeextremej/src/Solution.java
+++ ieeextremej/src/Solution.java
@@ -4,6 +4,17 @@
  * @author kommusoft
  */
 public class Solution {
+    
+    private class FS {
+        
+        public int dist;
+        public int cost;
+        
+        public FS (int dist, int cost) {
+            
+        }
+        
+    }
 
     public static void main(String[] args) {
         new Solution().run();
