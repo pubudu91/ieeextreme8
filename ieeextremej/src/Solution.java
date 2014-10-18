4842ab90e9ce418400f2df3e72cd1124250e8af2
KommuSoft <vanonsem.willem@gmail.com>
2014-10-18 20:23:42 +0200

    ... and another commit ...

--- ieeextremej/src/Solution.java
+++ ieeextremej/src/Solution.java
@@ -7,14 +7,13 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author kommusoft
  */
 public class Solution {
-    
-    public static void main (String[] args) {
+
+    public static void main(String[] args) {
         new Solution().run();
     }
 
@@ -23,14 +22,25 @@ public class Solution {
         int r = sc.nextInt();
         int c = sc.nextInt();
         int[] rs = new int[r];
-        for(int i = 0; i < r; i++) {
-            rs[i] = Integer.parseInt(sc.nextLine().replaceAll(" ",""),0x02);
+        for (int i = 0; i < r; i++) {
+            rs[i] = Integer.parseInt(sc.nextLine().replaceAll(" ", ""), 0x02);
         }
         int k = 0x00;
-        ArrayList<Integer> err = new ArrayList<Integer>();
-        for(int i1 = 0x00; i1 < r-0x01; i1++) {
-            int res = (rs[i1]^rs[i1+0x01])&(~(rs[i1+0x01]^rs[r-0x01]));
+        ArrayList<Integer> err1 = new ArrayList<Integer>();
+        for (int i1 = 0x00; i1 < r - 0x01; i1++) {
+            int res = (rs[i1] ^ rs[i1 + 0x01]) & (~(rs[i1 + 0x01] ^ rs[r - 0x01]));
+            if (res == 0x00) {
+                err1.add(i1);
+            }
+        }
+        for (int i1 = 0x00; i1 < r - 0x02; i1++) {
+            for (int i2 = i1+0x01; i2 < r - 0x01; i1++) {
+                int res = (rs[i1] ^ rs[i1 + 0x01]) & (~(rs[i1 + 0x01] ^ rs[r - 0x01]));
+                if (res == 0x00) {
+                    err1.add(i1);
+                }
+            }
         }
     }
-    
+
 }
