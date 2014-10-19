ab0bf96e9be4a7e49f3c89e529146018efa7fa68
KommuSoft <vanonsem.willem@gmail.com>
2014-10-19 18:20:14 +0200

    temp commit (by cron)

--- ieeextremej/src/Solution.java
+++ ieeextremej/src/Solution.java
@@ -1,6 +1,6 @@
 
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Scanner;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,12 +18,36 @@ public class Solution {
     }
 
     private void run() {
-        //System.out.println(score("3","3"));
-        //Sytem.out.println(score("7","7"));
-        //System.out.println(score("R","R"));
-        //System.out.println(score("T","T"));
-        //Scanner sc = new Scanner(System.in);
-        System.out.println(levenshtein(9, new int[]{6, 3, 7, 4, 2, 20, 17, 16, 10}, new int[]{3, 5, 4, 7, 50, 20, 16, 17, 10}));
+        /*System.out.println(score("A", "A"));
+         System.out.println(score("2", "2"));
+         System.out.println(score("3", "3"));
+         System.out.println(score("4", "4"));
+         System.out.println(score("5", "5"));
+         System.out.println(score("6", "6"));
+         System.out.println(score("7", "7"));
+         System.out.println(score("8", "8"));
+         System.out.println(score("9", "9"));
+         System.out.println(score("T", "T"));
+         System.out.println(score("J", "J"));
+         System.out.println(score("Q", "Q"));
+         System.out.println(score("K", "K"));*/
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int n = sc.nextInt();
+            if (n <= 0) {
+                return;
+            }
+            sc.nextLine();
+            String[] la = sc.nextLine().split(" ");
+            String[] lb = sc.nextLine().split(" ");
+            int[] va = new int[la.length];
+            int[] vb = new int[lb.length];
+            for (int i = 0; i < n; i++) {
+                va[i] = parse(la[i]);
+                vb[i] = parse(lb[i]);
+            }
+            System.out.println(levenshtein(n, va, vb));
+        }
     }
 
     public int parse(String c) {
@@ -46,7 +70,7 @@ public class Solution {
     }
 
     public int levenshtein(int n, int[] ra, int[] rb) {
-        int[][] matrix = new int[n + 2][n + 2];
+        int[][] matrix = new int[n + 1][n + 1];
         for (int i = 1; i <= n; i++) {
             matrix[0][i] = 0;
             matrix[i][0] = 0;
@@ -54,26 +78,36 @@ public class Solution {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 int dr = matrix[i - 0x01][j - 0x01];
-                int cost = dr + score(ra[i - 0x01], rb[j - 0x01]);
-                cost = Math.max(cost, matrix[i - 0x01][j]);
-                cost = Math.max(cost, matrix[i][j - 0x01]);
-                matrix[i][j] = cost;
+                int rai = ra[i - 0x01];
+                int rbj = rb[j - 0x01];
+                //System.out.println(Arrays.deepToString(matrix));
+                int costa = dr + score(rai, rbj);
+                int costb = matrix[i - 0x01][j];
+                int costc = matrix[i][j - 0x01];
+                matrix[i][j] = Math.max(costa, Math.max(costb, costc));
             }
         }
-        System.out.println(Arrays.deepToString(matrix).replace("], [", "],\n["));
-        return matrix[n][n];
+        return 0x02 * matrix[n][n];
     }
 
     public int score(String a, String b) {
         return score(parse(a), parse(b));
     }
+    
+    public int sinscore (int a) {
+        if(a >= 15 && a <= 17) {
+            return 15;
+        } else {
+            return a;
+        }
+    }
 
     public int score(int a, int b) {
         if (a == 50 || b == 50) {
-            return Math.min(a, b);
+            return Math.min(sinscore(a),sinscore(b));
         } else if (a == b) {
             if (a != 20) {
-                return Math.min(15, a);
+                return sinscore(a);
             } else {
                 return 20;
             }
