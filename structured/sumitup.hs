1410a9ee9c2b95c74480ce924a513cab0d11c275
KommuSoft <vanonsem.willem@gmail.com>
2014-10-20 14:54:01 +0200

    added 'sum it up' markdown

---
+++ structured/sumitup.hs
@@ -0,0 +1,23 @@
+rest :: Int
+rest = 1000000007
+
+siu :: [Int] -> Int -> Int
+siu as n = mod ((powm n 2) * (foldl summ 0 as)) rest
+
+summ :: Int -> Int -> Int
+summ a b = mod (a+b) rest
+
+powm :: Int -> Int -> Int
+powm 0 _ = 1
+powm n k | even n = pw
+         | otherwise = mod (k*pw) rest
+    where nb = div n 2
+          kb = mod (k*k) rest
+          pw = powm nb kb
+
+main = do
+    a <- getLine
+    b <- getLine
+    let bints = map read $ words b
+    c <- readLn
+    print (siu bints c)
