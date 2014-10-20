9d6abec12c536189e4d068d9babb40c547929369
KommuSoft <vanonsem.willem@gmail.com>
2014-10-20 22:17:26 +0200

    all questions

--- structured/makebook.sh
+++ structured/makebook.sh
@@ -3,7 +3,10 @@ do
 	bn=$(basename "$f" ".md")
 	echo '\newpage' >> problemset.tex
 	pandoc -f markdown -t latex "$f" >> problemset.tex
-	echo "\subsection{Source code}\lstinputlisting[language=Haskell]{$bn.hs}" >> problemset.tex
+	if [ -e "$bn.hs" ]
+	then
+		echo "\subsection{Source code}\lstinputlisting[language=Haskell]{$bn.hs}" >> problemset.tex
+	fi
 done
 pdflatex --interaction batchmode book.tex
 pdflatex --interaction batchmode book.tex
